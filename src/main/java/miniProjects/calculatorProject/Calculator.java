42d7d0adac181dae37b6a37ec6a2d6f4fa4346a1
ElhamMoharrami <elham.moharrami.em@gmail.com>
2025-09-19 13:25:19 +0330

    Initial commit

---
+++ src/main/java/miniProjects/calculatorProject/Calculator.java
@@ -0,0 +1,41 @@
+package miniProjects.calculatorProject;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        runConsoleCalculator();
+    }
+    
+    private static void runConsoleCalculator() {
+        Scanner scanner = new Scanner(System.in);
+        double firstNumber, secondNumber, result=0;
+        char operator;
+
+        System.out.println("Enter first number: ");
+        firstNumber = scanner.nextDouble();
+
+        System.out.println("Enter operator (+,-,*,/) :");
+        operator = scanner.next().charAt(0);
+
+        System.out.println("Enter second number: ");
+        secondNumber = scanner.nextDouble();
+
+        switch (operator) {
+            case '+': result = firstNumber + secondNumber; break;
+            case '-': result = firstNumber - secondNumber; break;
+            case '*': result = firstNumber * secondNumber; break;
+            case '/':
+                if(secondNumber !=0 ) result=firstNumber/secondNumber;
+                else{
+                    System.out.println("Error: Division by zero!");
+                    return;
+                }
+                break;
+                default:
+                    System.out.println("Error: Invalid operator!");
+                    return;
+        }
+        System.out.println("Result: " + result);
+    }
+}
---
+++ src/main/java/shorts/ExplicitVsImplicitType.java
@@ -0,0 +1,10 @@
+package shorts;
+
+public class ExplicitVsImplicitType {
+    public static void main(String[] args) {
+      int num=9;
+      String name="Java";
+      var num1=88;
+      var name1="Java";
+    }
+}
